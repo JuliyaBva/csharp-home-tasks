cdf5b12d0ef07679f7d010d5fef622d37f6c1051
JuliyaBva <yuliyab8508@gmail.com>
2020-07-19 16:03:32 +0300

    commit home task04.1

---
+++ HomeWork04/Example_with_for/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace HomeWork04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Example with for:
+                //display text with infromation which number should be entered on screen
+                //add an ability to enter required number on screen
+                //add a cycle to check whether entered number > 0
+                //display success message if entered number > 0 and break the cycle
+                //display warning message if entered number <= 0
+                Console.OutputEncoding = Encoding.UTF8;
+
+                Console.WriteLine("Please enter integer number which more than 0: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; ; i++)
+                {
+                    if (number > 0)
+                    {
+                        Console.WriteLine("Number {0} is correct", number);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number {0} is not correct", number);
+                        break;
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HomeWork04/Example_with_while/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Example_with_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                Console.WriteLine("Please enter integer number which more than 0: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                if (number1 > 0)
+                {
+                    Console.WriteLine("Number {0} is correct", number1);
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+        }
+    }
+}
