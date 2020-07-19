6425065f3a2699683cd43313828c165df3545583
JuliyaBva <yuliyab8508@gmail.com>
2020-07-19 19:04:05 +0300

    commit home task 4,2

---
+++ HomeTask4.2/HomeTask4.2/Program.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+
+namespace HomeTask4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeNumber;
+            bool isNumberPrime;
+            for (primeNumber = 2; primeNumber < 200; primeNumber++)
+            {
+                isNumberPrime = true;
+                for (int i = 2; i <= primeNumber / 2; i++)
+                {
+                    if ((primeNumber % i) == 0)
+                    {
+                        isNumberPrime = false;
+                    }
+                }
+                if (isNumberPrime)
+                    Console.WriteLine(primeNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
