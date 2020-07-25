6905cbec5ba0d24e597b63219228d8a2a7d4e6e0
JuliyaBva <yuliyab8508@gmail.com>
2020-07-26 01:31:48 +0300

    commit home task 5

--- HomeWork5.2_Strs/HomeWork5.2_Strs/Program.cs
+++ HomeWork5.2_Strs/HomeWork5.2_Strs/Program.cs
@@ -6,29 +6,18 @@ namespace HomeWork5._2_Strs
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter word which contain 'a' letters: ");
+            Console.Write("Please enter word which contains 'a' letter: ");
             string word = Console.ReadLine();
-            string str = word;
-            FindAllChar('a', str);
-        }
-        static void FindAllChar(Char target, String enteredWord)
-        {
-            int startIndex = -1;
-            int hitCount = 0;
-            // Search for all occurrences of the target.
-            while (true)
+            char[] array = word.ToCharArray();
+            Console.WriteLine("New word:");
+            for (int i = 0; i < array.Length; i++)
             {
-                startIndex = enteredWord.IndexOf(
-                    target, startIndex + 1,
-                    enteredWord.Length - startIndex - 1);
-                // Exit the loop if the target is not found.
-                if (startIndex < 0)
-                    break;
-                hitCount++;
+                if (array[i] == 'a')
+                {
+                    array[i] = 'A';
+                }
+                Console.Write(array[i]);
             }
-            Console.WriteLine("occurrences of 'a' in {0}: {1}", enteredWord, hitCount);
-
-
         }
     }
 }
