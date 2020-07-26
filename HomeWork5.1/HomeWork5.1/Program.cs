3ffa87279f08b50345eabaf805373b406b8e5eb8
JuliyaBva <yuliyab8508@gmail.com>
2020-07-26 17:48:26 +0300

    commit home task 5.1

--- HomeWork5.1/HomeWork5.1/Program.cs
+++ HomeWork5.1/HomeWork5.1/Program.cs
@@ -6,12 +6,11 @@ namespace HomeWork5._1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
-
+            const int n = 9;
+            int[] array = new int[n] { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
             int max = int.MinValue;
             foreach (int number in array)
             {
-       
              if (number < 0 && number > max) max = number;
             }
             Console.WriteLine("Number of the last negative value of the given array is {0}", max);
